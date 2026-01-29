61c91fd973d3ff9a488ba3ac955ffbbd3cff3ca4
Ian Lindley Del Rosario <ianlindley0429@gmail.com>
2026-01-29 19:50:35 +0800

    creted c and cpp files for final tasks of tech3

---
+++ tech3.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+int main() {
+    x = 21;
+    
+    int x;
+
+    x = 42;
+
+    printf("%d\n", x);
+
+    return 0;
+}
---
+++ tech3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;  
+
+int main(){
+x = 21;
+
+int x;
+
+x = 42;
+
+cout << x << endl;
+
+    return 0;
+}
