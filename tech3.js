744a07264ed8a61dbe35b1a32071a3caeb083f0d
Ian Lindley Del Rosario <ianlindley0429@gmail.com>
2026-01-29 19:32:58 +0800

    fixed tech3 jscript

--- tech3.js
+++ tech3.js
@@ -3,15 +3,14 @@ function function1() {
         function function2() {
             for (let i2 = 0; i2 < 100; i2 += 3) {
                 function function3() {
-                    for (let i3 = 0; i3 < 10; i3 += 1) {
+                    for (let i3 = 0; i3 < 100; i3 += 1) {
                         if (i === i2 && i2 === i3) {
                             console.log(i);
                         }
                     }
-                    function3();
-                }
+                }function3();
             }
-            function2();
-        }
+            
+        }function2();
     }
-}
+}function1();
